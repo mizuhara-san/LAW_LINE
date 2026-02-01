6a4e84a58d8dc413c5364af7ef28d837622866ba
mizuhara-san <minatobito55@gmail.com>
2026-02-01 10:39:32 +0530

    add config for deployment

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const CONFIG = {
+    GEMINI_API_KEY: "AIzaSyDcuBcTtTuMS_w7M_4HcXkgM6U7ze8mogQ",
+    FIREBASE_API_KEY: "AIzaSyD0g4Xc2KoshSSSZoHBQAvTZss4JzuZSQQ"
+};
+
+export default CONFIG;
